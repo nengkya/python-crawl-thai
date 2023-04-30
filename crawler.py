253c286c960c9d5082ebf8c6658b1c7ac2ea7dd3
nengkya <nengkya@yahoo.com>
2023-04-30 16:45:39 +0700

    readme.md add links

--- crawler.py
+++ crawler.py
@@ -1,2 +1,14 @@
+class Crawler:
+	def __init__(self, urls=[]):
+		self.visited_urls=[]
+		self.urls_to_visit = urls
+
+	def run(self):
+		while self.urls_to_visit:
+			#pop first element of the urls list
+			url = self.urls_to_visit.pop(0)
+
+
+
 if __name__ == '__main__':
-	Crawler().run()
+	Crawler(urls=['https://www.customs.go.th/statistic_report.php?lang=en&']).run()
