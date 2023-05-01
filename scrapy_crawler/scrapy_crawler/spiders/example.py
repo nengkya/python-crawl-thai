e76479db2bf750650fb01383674a1576970df353
HaGa IT Solution <nengkya@yahoo.com>
2023-05-01 09:12:02 +0700

    Update example.py

--- scrapy_crawler/scrapy_crawler/spiders/example.py
+++ scrapy_crawler/scrapy_crawler/spiders/example.py
@@ -5,7 +5,7 @@ class ExampleSpider(scrapy.Spider):
     name = "example"
     allowed_domains = ["customs.go.th"]
     start_urls = ["https://www.customs.go.th/statistic_report.php?show_search=1&s=SMy1GW7C8PGzeBj3"]
-    rules
+    rules = (LinkExtractor())
 
     def parse(self, response):
         pass
