bbcc0f5fd14b24d4d62df6a3c428ae15bd0fa36d
HaGa IT Solution <nengkya@yahoo.com>
2023-05-01 09:06:44 +0700

    Update example.py

--- scrapy_crawler/scrapy_crawler/spiders/example.py
+++ scrapy_crawler/scrapy_crawler/spiders/example.py
@@ -4,7 +4,8 @@ import scrapy
 class ExampleSpider(scrapy.Spider):
     name = "example"
     allowed_domains = ["customs.go.th"]
-    start_urls = ["http://customs.go.th/"]
+    start_urls = ["https://www.customs.go.th/statistic_report.php?show_search=1&s=SMy1GW7C8PGzeBj3"]
+    rules
 
     def parse(self, response):
         pass
