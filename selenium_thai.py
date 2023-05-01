cc7b9158a7a55e1e9528db83007715bb03d2fb30
nengkya <nengkya@yahoo.com>
2023-05-01 10:55:08 +0700

    selenium input text

--- selenium_thai.py
+++ selenium_thai.py
@@ -1,3 +1,43 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-driver = webdriver.Edge()
+#import Action chains for pause
+from selenium.webdriver.common.action_chains import ActionChains
+
+def test_eight_components():
+	driver = webdriver.Chrome()
+
+	driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+	title = driver.title
+	assert title == "Web form"
+
+	#driver.implicitly_wait(0.5)
+
+	text_box = driver.find_element(by=By.NAME, value="my-text")
+
+	submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+
+	#create action chain object
+	action = ActionChains(driver)
+
+	action.pause(15)
+
+	#perform the operation
+	action.perform()
+
+	text_box.send_keys("Selenium")
+	submit_button.click()
+
+	message = driver.find_element(by=By.ID, value="message")
+	value = message.text
+	assert value == "Received!"
+
+	driver.quit()
+
+
+if __name__ == '__main__':
+	os.system('tput reset')
+
+	test_eight_components()
